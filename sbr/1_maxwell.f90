7cbf3308f5428c588dfd0dbd96144409e2ee6c2d
nteplova <85991051+nteplova@users.noreply.github.com>
2026-02-11 10:47:33 +0300

    grids old

--- sbr/1_maxwell.f90
+++ sbr/1_maxwell.f90
@@ -3,7 +3,7 @@ module maxwell
       use kind_module      
       use constants, only : zero, pisqrt, pi2sqrt, pqe
       implicit none
-      integer, parameter :: i0 = 5010
+      integer, parameter :: i0 = 1002
 
 
       logical flag_d0
